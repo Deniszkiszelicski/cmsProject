e5d32e8c4355f2dac321e2807feef336194c5c15
pavel <pavel.vabishchevich@gmail.com>
2017-01-26 13:41:39 +0100

    rename page2 to Content and add URL and Duration fields

--- imports/api/content/methods.js
+++ imports/api/content/methods.js
@@ -2,10 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
 Meteor.methods({
+  //  TODO: Add check for duration of videos
   addContent: function (content) {
     check(content.name, String);
     check(content.type, String);
-    Content.insert({ name: content.name, type: content.type });
+    check(content.URL, String);
+    Content.insert({ name: content.name, type: content.type, URL: content.URL, duration: content.duration });
     console.log('inserted into Content collection');
   },
   deleteContent: function(id) {
--- imports/ui/components/contentForm.js
+++ imports/ui/components/contentForm.js
@@ -8,6 +8,6 @@ Meteor.subscribe('content');
 Template.contentForm.events({
   'submit .content-form': function addContent(event) {
     event.preventDefault();
-    Meteor.call('addContent', { name: $('#contentName').val(), type: $('#contentType').val() });
+    Meteor.call('addContent', { name: $('#contentName').val(), type: $('#contentType').val(), URL: $('#contentURL').val(), duration: $('#contentDuration').val() });
   },
 });
