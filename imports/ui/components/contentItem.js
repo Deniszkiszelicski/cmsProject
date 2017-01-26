ca0e5543e3248989a927f3ad3089f53cefdd9c4d
pavel <pavel.vabishchevich@gmail.com>
2017-01-26 17:09:27 +0100

    add editing form and close it when btn clicked without saving yet

--- imports/ui/components/contentItem.js
+++ imports/ui/components/contentItem.js
@@ -1,12 +1,29 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import './contentItem.html';
 
-Meteor.subscribe('content');
+
+Template.contentItem.onCreated(function onCreated() {
+  this.isEditMode = new ReactiveVar(false);
+});
 
 Template.contentItem.events({
   'click #button-delete-content': function deleteContent(event) {
     event.preventDefault();
-    console.log('button delete was clicked');
     Meteor.call('deleteContent', this._id);
+  },
+  'click #button-edit-content': function editContent(event, templateInstance) {
+    event.preventDefault();
+    templateInstance.isEditMode.set(true);
+  },
+  'click #button-close-content-form': function closeForm(event, templateInstance) {
+    event.preventDefault();
+    templateInstance.isEditMode.set(false);
+  },
+});
+
+Template.contentItem.helpers({
+  isEditMode: function isEditMode() {
+    return Template.instance().isEditMode.get();
   },
 });
