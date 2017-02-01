c86e60a4e892143fa7d43cab2c0735499b70b513
pavel <pavel.vabishchevich@gmail.com>
2017-02-01 11:16:10 +0100

    fix netId missing for CM and subCm

--- imports/ui/pages/admin_role.js
+++ imports/ui/pages/admin_role.js
@@ -22,11 +22,11 @@ Template.admin_role.events({
     roleName: "admin", createdNet: $('#create_net').is(":checked"), createPlayer: $('#create_player').is(":checked"), editPlayer: $('#edit_player').is(":checked"), deletePlayer: $('#delete_player').is(":checked"),
     addVideoPic: $('#add_videoPic').is(":checked"), editVideoPic: $('#edit_videoPic').is(":checked"), deleteVideoPic: $('#delete_videoPic').is(":checked") });
 
-    Meteor.call('createRole', { netName: $('#cmnetName').val(), netId: $('#cmnetId').val(),
+    Meteor.call('createRole', { netName: $('#netName').val(), netId: $('#netId').val(),
     roleName: "contentManager", createdNet: $('#cmcreate_net').is(":checked"), createPlayer: $('#cmcreate_player').is(":checked"), editPlayer: $('#cmedit_player').is(":checked"), deletePlayer: $('#cmdelete_player').is(":checked"),
     addVideoPic: $('#cmadd_videoPic').is(":checked"), editVideoPic: $('#cmedit_videoPic').is(":checked"), deleteVideoPic: $('#cmdelete_videoPic').is(":checked") });
 
-    Meteor.call('createRole', { netName: $('#scmnetName').val(), netId: $('#scmnetId').val(),
+    Meteor.call('createRole', { netName: $('#netName').val(), netId: $('#netId').val(),
     roleName: "subContentManager", createdNet: $('#scmcreate_net').is(":checked"), createPlayer: $('#scmcreate_player').is(":checked"), editPlayer: $('#scmedit_player').is(":checked"), deletePlayer: $('#scmdelete_player').is(":checked"),
     addVideoPic: $('#scmadd_videoPic').is(":checked"), editVideoPic: $('#scmedit_videoPic').is(":checked"), deleteVideoPic: $('#scmdelete_videoPic').is(":checked") });
 
