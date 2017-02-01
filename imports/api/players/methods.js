53974d98a310bc6e65c3cfe740f76ff777c1e386
pavel <pavel.vabishchevich@gmail.com>
2017-02-01 15:06:07 +0100

    for saving only

--- imports/api/players/methods.js
+++ imports/api/players/methods.js
@@ -3,7 +3,7 @@ import { check } from 'meteor/check';
 
 Meteor.methods({
   //  TODO: Add check for remaining fields
-  addPlayer: function (player) {
+  upsertPlayer: function (player) {
     check(player.name, String);
     check(player.playerId, String);
     check(player.location, String);
@@ -12,6 +12,7 @@ Meteor.methods({
     check(player.townCity, String);
     check(player.district, String);
     check(player.playTime, String);
+    console.log('before update');
     Players.update({ _id: player._id },
       { name: player.name,
         playerId: player.playerId,
@@ -20,7 +21,36 @@ Meteor.methods({
         postIndex: player.postIndex,
         townCity: player.townCity,
         district: player.district,
-        playTime: player.playTime },
+        playTime: player.playTime,
+
+        mondayStart1: player.mondayStart1,
+        mondayEnd1: player.mondayEnd1,
+        mondayStart2: player.mondayStart2,
+        mondayEnd2: player.mondayEnd2,
+        tuesdayStart1: player.tuesdayStart1,
+        tuesdayEnd1: player.tuesdayEnd1,
+        tuesdayStart2: player.tuesdayStart2,
+        tuesdayEnd2: player.tuesdayEnd2,
+        wednesdayStart1: player.wednesdayStart1,
+        wednesdayEnd1: player.wednesdayEnd1,
+        wednesdayStart2: player.wednesdayStart2,
+        wednesdayEnd2: player.wednesdayEnd2,
+        thursdayStart1: player.thursdayStart1,
+        thursdayEnd1: player.thursdayEnd1,
+        thursdayStart2: player.thursdayStart2,
+        thursdayEnd2: player.thursdayEnd2,
+        fridayStart1: player.fridayStart1,
+        fridayEnd1: player.fridayEnd1,
+        fridayStart2: player.fridayStart2,
+        fridayEnd2: player.fridayEnd2,
+        saturdayStart1: player.saturdayStart1,
+        saturdayEnd1: player.saturdayEnd1,
+        saturdayStart2: player.saturdayStart2,
+        saturdayEnd2: player.saturdayEnd2,
+        sundayStart1: player.sundayStart1,
+        sundayEnd1: player.sundayEnd1,
+        sundayStart2: player.sundayStart2,
+        sundayEnd2: player.sundayEnd2 },
         { upsert: true, multi: false });
   },
   deletePlayer: function(id) {
--- imports/ui/components/players/playerForm.js
+++ imports/ui/components/players/playerForm.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 import './playerForm.html';
 import '../../../api/players/methods';
 import '../../../api/players/collection';
@@ -6,8 +7,9 @@ import '../../../api/players/collection';
 Meteor.subscribe('players');
 
 Template.playerForm.events({
-  'submit .player-form': function addPlayer(event) {
+  'submit .player-form': function upsertPlayer(event) {
     event.preventDefault();
+    console.log('monday form input', $('#tuesdayEnd2').val());
     Meteor.call('addPlayer',
       { _id: this._id,
         name: event.currentTarget.name.value,
@@ -17,6 +19,35 @@ Template.playerForm.events({
         postIndex: event.currentTarget.postIndex.value,
         townCity: event.currentTarget.townCity.value,
         district: event.currentTarget.district.value,
-        playTime: event.currentTarget.playTime.value });
+        // Play time hours begin
+        mondayStart1: event.currentTarget.mondayStart1.value,
+        mondayEnd1: event.currentTarget.mondayEnd1.value,
+        mondayStart2: event.currentTarget.mondayStart2.value,
+        mondayEnd2: event.currentTarget.mondayEnd2.value,
+        tuesdayStart1: event.currentTarget.tuesdayStart1.value,
+        tuesdayEnd1: event.currentTarget.tuesdayEnd1.value,
+        tuesdayStart2: event.currentTarget.tuesdayStart2.value,
+        tuesdayEnd2: event.currentTarget.tuesdayEnd2.value,
+        wednesdayStart1: event.currentTarget.wednesdayStart1.value,
+        wednesdayEnd1: event.currentTarget.wednesdayEnd1.value,
+        wednesdayStart2: event.currentTarget.wednesdayStart2.value,
+        wednesdayEnd2: event.currentTarget.wednesdayEnd2.value,
+        thursdayStart1: event.currentTarget.thursdayStart1.value,
+        thursdayEnd1: event.currentTarget.thursdayEnd1.value,
+        thursdayStart2: event.currentTarget.thursdayStart2.value,
+        thursdayEnd2: event.currentTarget.thursdayEnd2.value,
+        fridayStart1: event.currentTarget.fridayStart1.value,
+        fridayEnd1: event.currentTarget.fridayEnd1.value,
+        fridayStart2: event.currentTarget.fridayStart2.value,
+        fridayEnd2: event.currentTarget.fridayEnd2.value,
+        saturdayStart1: event.currentTarget.saturdayStart1.value,
+        saturdayEnd1: event.currentTarget.saturdayEnd1.value,
+        saturdayStart2: event.currentTarget.saturdayStart2.value,
+        saturdayEnd2: event.currentTarget.saturdayEnd2.value,
+        sundayStart1: event.currentTarget.sundayStart1.value,
+        sundayEnd1: event.currentTarget.sundayEnd1.value,
+        sundayStart2: event.currentTarget.sundayStart2.value,
+        sundayEnd2: event.currentTarget.sundayEnd2.value });
+        // Play time hours end
   },
 });
