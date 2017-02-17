66b0e8076a86cf3a00de6cbd9107db816595e822
Pavel <pavel.vabishchevich@gmail.com>
2017-02-17 14:17:41 +0100

    add upload video in media

--- imports/api/medien/collection.js
+++ imports/api/medien/collection.js
@@ -57,3 +57,112 @@ Images = new FilesCollection({
     console.log("inserted id = ", fileRef._id);
   },
 });
+
+// Videos = new FilesCollection({
+//   // storagePath: 'assets/app/uploads/Videos',
+//   // downloadRoute: '/files/videos'
+//   collectionName: 'Videos',
+//   // chunkSize: 1024*2048,
+//   // throttle: 1024*512,
+//   // permissions: 0755,
+//   allowClientCode: false,
+//   // cacheControl: 'public, max-age=31536000',
+//   onbeforeunloadMessage: function () {
+//     return 'Upload is still in progress! Upload will be aborted if you leave this page!';
+//   },
+//   onBeforeUpload: function (file) {
+//     if (file.size <= 104857600 && /mp4/i.test(file.ext)) {
+//       return true;
+//     } else {
+//       return 'Please upload video, with size equal or less than 100MB';
+//     }
+//   },
+//   onAfterUpload: function (fileRef) {
+//     console.log("inserted id = ", fileRef._id);
+//   },
+//   // downloadCallback: function (fileObj) {
+//   //   if (this.params.query.download == 'true') {
+//   //     Videos.update(fileObj._id, {$inc: {'meta.downloads': 1}});
+//   //   }
+//   //   // Must return true to continue download
+//   //   return true;
+//   // },
+//   // protected: function (fileObj) {
+//   //   // Check if user is own this file
+//   //   if (fileObj.meta.owner === this.userId) {
+//   //     return true;
+//   //   } else {
+//   //     return false;
+//   //   }
+//   // }
+// });
+
+
+Videos = new FilesCollection({
+  // debug: true,
+  // throttle: false,
+  // chunkSize: 1024*1024,
+  storagePath: 'assets/app/uploads/videos',
+  // collectionName: 'uploadedFiles',
+  collectionName: 'Videos',
+  allowClientCode: true,
+  protected(fileObj) {
+    if (fileObj) {
+      if (!(fileObj.meta && fileObj.meta.secured)) {
+        return true;
+      } else if ((fileObj.meta && fileObj.meta.secured === true) && this.userId === fileObj.userId) {
+        return true;
+      }
+    }
+    return false;
+  },
+  onBeforeRemove(cursor) {
+    const res = cursor.map((file) => {
+      if (file && file.userId && _.isString(file.userId)) {
+        return file.userId === this.userId;
+      }
+      return false;
+    });
+    return !~res.indexOf(false);
+  },
+  onBeforeUpload() {
+    if (this.file.size <= 1024 * 1024 * 128) {
+      return true;
+    }
+    return "Max. file size is 128MB you've tried to upload " + (filesize(this.file.size));
+  },
+  downloadCallback(fileObj) {
+    if (this.params && this.params.query && this.params.query.download === 'true') {
+      Videos.collection.update(fileObj._id, {
+        $inc: {
+          'meta.downloads': 1
+        }
+      });
+    }
+    return true;
+  },
+  interceptDownload(http, fileRef, version) {
+    if (useDropBox || useS3) {
+      const path = (fileRef && fileRef.versions && fileRef.versions[version] && fileRef.versions[version].meta && fileRef.versions[version].meta.pipeFrom) ? fileRef.versions[version].meta.pipeFrom : void 0;
+      if (path) {
+        // If file is successfully moved to Storage
+        // We will pipe request to Storage
+        // So, original link will stay always secure
+
+        // To force ?play and ?download parameters
+        // and to keep original file name, content-type,
+        // content-disposition and cache-control
+        // we're using low-level .serve() method
+        this.serve(http, fileRef, fileRef.versions[version], version, Request({
+          url: path,
+          headers: _.pick(http.request.headers, 'range', 'accept-language', 'accept', 'cache-control', 'pragma', 'connection', 'upgrade-insecure-requests', 'user-agent')
+        }));
+        return true;
+      }
+      // While file is not yet uploaded to Storage
+      // We will serve file from FS
+      return false;
+    }
+    return false;
+  }
+});
--- imports/ui/components/medien/mediaForm.js
+++ imports/ui/components/medien/mediaForm.js
@@ -7,24 +7,26 @@ import './mediaForm.html';
 
 Meteor.subscribe('medien');
 Meteor.subscribe('files.images.all');
+Meteor.subscribe('files.videos.all');
 
 Template.mediaForm.onCreated(function () {
   this.currentUpload = new ReactiveVar(false);
   this.uploadedFileId = new ReactiveVar();
   this.uploadedFileName = new ReactiveVar();
-  // let uploadedFileName = "test2";
-  // console.log("mediaForm onCreated this._id = ", this._id);
-  // if(!!this._id) {
-  //   uploadedFileName = "test1";
-  // }
-  // this.uploadedFileName = new ReactiveVar(uploadedFileName);
+  this.uploadedFileType = new ReactiveVar();
 });
 
 Template.mediaForm.onRendered(function () {
   const fileId = this.data.fileId;
   if(!!fileId) {
-    const fileName = Images.findOne({ _id: fileId }).name;
-    // const category = Images.findOne({ _id: fileId }).name;
+    const type = this.data.type;
+    let fileName;
+    if (type.match(/image/)) {
+      fileName = Images.findOne({ _id: fileId }).name;
+    }
+    if (type.match(/video/)) {
+      fileName = Videos.findOne({ _id: fileId }).name;
+    }
     Template.instance().uploadedFileName.set(fileName);
     Template.instance().uploadedFileId.set(fileId);
   }
@@ -54,7 +56,7 @@ Template.mediaForm.events({
       { _id: this._id,
         name: $('#nameOfMedia').val(),
         category: $('#searchCategories').val(),
-        type: 'image',
+        type: templateInstance.uploadedFileType.get(),
         // createdAt: Date.now(),
         fileId: fileId });
   },
@@ -62,20 +64,33 @@ Template.mediaForm.events({
     if (e.currentTarget.files && e.currentTarget.files[0]) {
       // We upload only one file, in case
       // multiple files were selected
-      var upload = Images.insert({
-        file: e.currentTarget.files[0],
-        streams: 'dynamic',
-        chunkSize: 'dynamic'
-      }, false);
-
+      const type = e.currentTarget.files[0].type;
+      // var upload;
+      let upload;
+      if (type.match(/image/)) {
+        upload = Images.insert({
+          file: e.currentTarget.files[0],
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+        templateInstance.uploadedFileType.set("image");
+      }
+      if (type.match(/video/)) {
+        upload = Videos.insert({
+          file: e.currentTarget.files[0],
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+        templateInstance.uploadedFileType.set("video");
+      }
       upload.on('start', function () {
         templateInstance.currentUpload.set(this);
       });
-
       upload.on('end', function (error, fileObj) {
         templateInstance.uploadedFileId.set(fileObj._id);
         templateInstance.currentUpload.set(false);
         templateInstance.uploadedFileName.set(fileObj.name);
+        console.log("on end of upload, fileObje = ", fileObj);
       });
       upload.start();
     }
