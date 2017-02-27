c311b6c19ee7ac97a590683c9dced1bebe80f49f
Pavel <pavel.vabishchevich@gmail.com>
2017-02-27 14:40:40 +0100

    display CG names in the list of playlists

--- imports/ui/components/playlists/playlistsList.js
+++ imports/ui/components/playlists/playlistsList.js
@@ -3,3 +3,22 @@ import '../../../api/playlists/methods';
 import '../../../api/playlists/collection';
 import './playlistsList.html';
 import './playlist';
+
+Meteor.subscribe('playlists');
+
+Template.playlistsList.onCreated(function () {
+  this.filterText = new ReactiveVar();
+});
+
+Template.playlistsList.helpers({
+  filteredPlaylists: () => {
+    let filterText = Template.instance().filterText.get();
+    return Playlists.find({playerId: { $regex: new RegExp(filterText), $options: 'i' }}).fetch();
+  },
+});
+
+Template.playlistsList.events({
+  'keyup #playlist-filter-input': function setFilterText(event, templateInstance) {
+    templateInstance.filterText.set(event.currentTarget.value);
+  }
+});
--- imports/ui/pages/playlistsPage.js
+++ imports/ui/pages/playlistsPage.js
@@ -6,8 +6,28 @@ import './playlistsPage.html';
 
 Meteor.subscribe('playlists');
 
+Template.playlistsPage.onCreated(function () {
+  this.isCreateNew = new ReactiveVar(false);
+  this.currentPlaylist = new ReactiveVar();
+});
+
 Template.playlistsPage.helpers({
   isCreateNew: function isCreateNew () {
-    return true;
+    return Template.instance().isCreateNew.get();
+  },
+  getCurrentPlaylist: function getCurrentPlaylist() {
+    return Template.instance().currentPlaylist.get();
+  },
+});
+
+Template.playlistsPage.events({
+  'click .button-new, click #button-edit-playlist': function openPlaylistForm(event, templateInstance) {
+    event.preventDefault();
+    templateInstance.currentPlaylist.set(this);
+    templateInstance.isCreateNew.set(true);
+  },
+  'click #button-close-playlist-form, click #btn-save-playlist': function closePlaylistForm(event, templateInstance) {
+    event.preventDefault();
+    templateInstance.isCreateNew.set(false);
   },
 });
