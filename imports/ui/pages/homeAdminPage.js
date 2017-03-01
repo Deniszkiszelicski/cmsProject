9c69a745f461f20a4d233a973d29199bed6cb44f
Denisz <dencse@rocketmail.com>
2017-03-01 13:32:56 +0100

    richtext field added

--- imports/ui/pages/homeAdminPage.js
+++ imports/ui/pages/homeAdminPage.js
@@ -1,3 +1,26 @@
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 import { Meteor } from 'meteor/meteor';
+import { quilljs } from 'meteor/themeteorites:quilljs';
+
 import './homeAdminPage.html';
+
+Template.homeAdminPage.onRendered(function(){
+
+  var fullEditor;
+  fullEditor = new Quill('#full-editor', {
+    modules: {
+      // 'authorship': {
+      //   authorId: 'test', //Meteor.user().profile.user_name,
+      //   enabled: true
+      // },
+      'multi-cursor': true,
+      'toolbar': {
+        container: '#full-toolbar'
+      },
+      'link-tooltip': true
+    },
+    theme: 'snow'
+  });
+  return fullEditor;
+});
