5ab5da76a5611a5a3aacaccd14123a8b3a449e9a
Denisz <dencse@rocketmail.com>
2017-03-02 16:16:27 +0100

    befor merge

--- imports/ui/layouts/partials/footer.js
+++ imports/ui/layouts/partials/footer.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
 import './footer.html';
+
+
+Meteor.subscribe('networks');
+
+Template.footer.helpers({
+  footerLogo: () => {
+    return Networks.findOne({_id:Session.get("currentNetworkId")});
+
+  },
+
+
+});
