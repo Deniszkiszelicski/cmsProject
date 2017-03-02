86aa9edbb50c3e8a1d98f0f124e94a544e39f44d
Pavel <pavel.vabishchevich@gmail.com>
2017-03-02 15:39:07 +0100

    link content regions and assort to network

--- imports/ui/layouts/partials/header.js
+++ imports/ui/layouts/partials/header.js
@@ -4,5 +4,11 @@ import './header.html';
 Template.header.helpers({
   showNetworkSelection: function showNetworkSelection() {
     return true;
-  }
+  },
+  currentUser: function displayCurrentUser () {
+    const user = Meteor.user();
+    if (user) {
+      return user.profile.name;
+    }
+  },
 });
