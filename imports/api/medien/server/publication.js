92dfbbe10f49697a23f1267a1acdf6d914a965e3
Pavel <pavel.vabishchevich@gmail.com>
2017-03-03 12:59:00 +0100

    enable seeMedia privilege

--- imports/api/medien/server/publication.js
+++ imports/api/medien/server/publication.js
@@ -1,7 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('medien', function() {
-  return Medien.find({});
+  const userId = this.userId;
+  if (userId) {
+    const roleId = Meteor.users.findOne({ _id: userId }).profile.role;
+    const role = Roles.findOne({ _id: roleId });
+    if (role) {
+      if (role.seeMedia) {
+        const networkId = role.networkId;
+        let roleIdsOfThisNetwork = Roles.find({ networkId: networkId }, { fields: { _id: 1 } }).fetch();
+        roleIdsOfThisNetwork = _(roleIdsOfThisNetwork).pluck("_id");
+        let userIdsOfThisNetwork = Meteor.users.find({ 'profile.role': { $in: roleIdsOfThisNetwork } }, { fields: { _id: 1 } }).fetch();
+        userIdsOfThisNetwork = _(userIdsOfThisNetwork).pluck("_id");
+        return Medien.find({ author: { $in: userIdsOfThisNetwork } });
+      } else {
+        return Medien.find({ author: userId });
+      }
+    }
+  }
 });
 
 // Meteor.publish('files.images.all', function () {
