1196951beeaa786772b3add5a710fc11ea7430c4
Pavel <pavel.vabishchevich@gmail.com>
2017-03-07 17:48:32 +0100

    add pagination and filter to list of players

--- imports/api/players/server/publication.js
+++ imports/api/players/server/publication.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish('players', function() {
-  return Players.find({});
+Meteor.publish('players', function(currentPage, showPerPage, filterText) {
+  if(currentPage && showPerPage) {
+    const skip = (currentPage - 1) * showPerPage;
+    if (filterText) {
+      return Players.find({ playerId: { $regex: new RegExp(filterText), $options: 'i' } }, { sort: { name: 1 }, skip: skip, limit: showPerPage});
+    } else {
+      return Players.find({}, { sort: { name: 1 }, skip: skip, limit: showPerPage});
+    }
+  }
 });
--- imports/ui/pages/playersPage.js
+++ imports/ui/pages/playersPage.js
@@ -8,6 +8,9 @@ import '../components/players/playersList';
 import './playersPage.html';
 
 Template.playersPage.onCreated(function onCreated() {
+  this.initialPage = new ReactiveVar(1);
+  this.initialRangeOfPages = new ReactiveVar([1, 2, 3, 4, 5]);
+
   this.isCreateNew = new ReactiveVar(false);
   this.currentPlayer = new ReactiveVar();
   Session.set("isDefaultPageLayout", true);
@@ -19,6 +22,15 @@ Template.playersPage.helpers({
   },
   getCurrentPlayer: function getPlayer() {
     return Template.instance().currentPlayer.get();
+  },
+  options: function getOptions() {
+    const initialPage = Template.instance().initialPage.get();
+    const initialRangeOfPages = Template.instance().initialRangeOfPages.get();
+    const options = { header: "List of all players", initialPage: initialPage,
+                      enableButtonNewPlayer: true, enableFilter: true,
+                      initialRangeOfPages: initialRangeOfPages };
+    const includedCGsWithOptions = { options: options};
+    return includedCGsWithOptions;
   },
 });
 
@@ -32,5 +44,45 @@ Template.playersPage.events({
   'click #button-close-player-form, click .button-save': function closeForm(event, templateInstance) {
     event.preventDefault();
     templateInstance.isCreateNew.set(false);
+  },
+  'click .pagination .page-number': function goToPage(event, templateInstance) {
+    const pageN = event.currentTarget.dataset.page;
+    templateInstance.initialPage.set(parseInt(pageN));
+  },
+  'click .pagination .page-go-forward': function goForward(event, templateInstance) {
+    const oldRangeOfPages = templateInstance.initialRangeOfPages.get();
+    let initialPage = templateInstance.initialPage.get();
+    const position = oldRangeOfPages.indexOf(initialPage);
+    let newRangeOfPages = oldRangeOfPages;
+    if (position < 4) {
+      initialPage = initialPage + 1;
+    }
+    if (position == 4) {
+      initialPage = initialPage + 1;
+      newRangeOfPages.push(oldRangeOfPages[4] + 1);
+      newRangeOfPages.shift();
+    }
+    templateInstance.initialRangeOfPages.set(newRangeOfPages);
+    templateInstance.initialPage.set(initialPage);
+  },
+  'click .pagination .page-go-back': function goBack(event, templateInstance) {
+    // const pageN = event.currentTarget.dataset.page;
+    // templateInstance.initialPage.set(pageN);
+    const initialPage = templateInstance.initialPage.get();
+    if (initialPage == 1) {
+      return 0;
+    }
+    const oldRangeOfPages = templateInstance.initialRangeOfPages.get();
+    const position = oldRangeOfPages.indexOf(initialPage);
+    let newRangeOfPages = oldRangeOfPages;
+    if (position > 0) {
+      templateInstance.initialPage.set(initialPage - 1);
+    }
+    if (position == 0) {
+      templateInstance.initialPage.set(initialPage - 1);
+      newRangeOfPages.unshift(oldRangeOfPages[0] - 1);
+      newRangeOfPages.pop();
+    }
+    templateInstance.initialRangeOfPages.set(newRangeOfPages);
   },
 });
