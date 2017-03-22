318a16216c98620345533abbde3f12f683c476f3
Pavel <pavel.vabishchevich@gmail.com>
2017-03-22 12:53:07 +0100

    add delete confirmation dialog to playlists

--- imports/ui/components/playlists/playlistsList.js
+++ imports/ui/components/playlists/playlistsList.js
@@ -1,13 +1,15 @@
 import { Template } from 'meteor/templating';
 import '../../../api/playlists/methods';
 import '../../../api/playlists/collection';
-import './playlistsList.html';
+import '../modals/deleteConfirmation';
 import './playlist';
+import './playlistsList.html';
 
 Meteor.subscribe('playlists');
 
 Template.playlistsList.onCreated(function () {
   this.filterText = new ReactiveVar();
+  this.playlistToDelete = new ReactiveVar();
 });
 
 Template.playlistsList.helpers({
@@ -15,10 +17,26 @@ Template.playlistsList.helpers({
     let filterText = Template.instance().filterText.get();
     return Playlists.find({ name: { $regex: new RegExp(filterText), $options: 'i' } }).fetch();
   },
+  playlistToDelete: function playlistToDelete() {
+    const playlist = Template.instance().playlistToDelete.get();
+    if (playlist) {
+      return "Delete '" + playlist.name + "' playlist.";
+    }
+  },
 });
 
 Template.playlistsList.events({
   'keyup #playlist-filter-input': function setFilterText(event, templateInstance) {
     templateInstance.filterText.set(event.currentTarget.value);
-  }
+  },
+  'click #button-delete-confired': function deletePlaylist(event, templateInstance) {
+    event.preventDefault();
+    const playlist = templateInstance.playlistToDelete.get();
+    templateInstance.playlistToDelete.set();
+    Meteor.call('deletePlaylist', playlist._id);
+  },
+  'click .glyphicon-trash': function deletePlaylist(event, templateInstance) {
+    event.preventDefault();
+    templateInstance.playlistToDelete.set(this);
+  },
 });
