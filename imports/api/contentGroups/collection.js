9fec1d3ce80fbb12b06277518447afddc4ea47bf
Pavel <pavel.vabishchevich@gmail.com>
2017-04-26 09:44:54 +0200

    implement cg visibility

--- imports/api/contentGroups/collection.js
+++ imports/api/contentGroups/collection.js
@@ -35,7 +35,7 @@ ContentGroupsSchema = new SimpleSchema({
       if (this.isInsert) {
         return new Date();
       } else if (this.isUpsert) {
-        return {$setOnInsert: new Date()};
+        return { $setOnInsert: new Date() };
       } else {
         this.unset();  // Prevent user from supplying their own value
       }
@@ -44,7 +44,13 @@ ContentGroupsSchema = new SimpleSchema({
   author: {
 		type: String,
 		autoValue: function() {
-			return this.userId;
+      if (this.isInsert) {
+        return this.userId;
+      } else if (this.isUpsert) {
+        return {$setOnInsert: this.userId};
+      } else {
+        this.unset();
+      }
 		}
 	},
 });
--- imports/api/contentGroups/server/publication.js
+++ imports/api/contentGroups/server/publication.js
@@ -6,12 +6,12 @@ Meteor.publish('contentGroups', function(currentPage, showPerPage, filterText) {
     const roleId = Meteor.users.findOne({ _id: userId }).profile.role;
     const role = Roles.findOne({ _id: roleId });
     if (role) {
-      if (role.grantContentGrpAccess || true) {
-        const networkId = role.networkId;
-        let roleIdsOfThisNetwork = Roles.find({ networkId: networkId }, { fields: { _id: 1 } }).fetch();
-        roleIdsOfThisNetwork = _(roleIdsOfThisNetwork).pluck("_id");
-        let userIdsOfThisNetwork = Meteor.users.find({ 'profile.role': { $in: roleIdsOfThisNetwork } }, { fields: { _id: 1 } }).fetch();
-        userIdsOfThisNetwork = _(userIdsOfThisNetwork).pluck("_id");
+      const networkId = role.networkId;
+      let roleIdsOfThisNetwork = Roles.find({ networkId: networkId }, { fields: { _id: 1 } }).fetch();
+      roleIdsOfThisNetwork = _(roleIdsOfThisNetwork).pluck("_id");
+      let userIdsOfThisNetwork = Meteor.users.find({ 'profile.role': { $in: roleIdsOfThisNetwork } }, { fields: { _id: 1 } }).fetch();
+      userIdsOfThisNetwork = _(userIdsOfThisNetwork).pluck("_id");
+      if (role.seeAllContentGroups) {
         if (currentPage && showPerPage) {
           const skip = (currentPage - 1) * showPerPage;
           if (filterText) {
@@ -28,12 +28,12 @@ Meteor.publish('contentGroups', function(currentPage, showPerPage, filterText) {
           const skip = (currentPage - 1) * showPerPage;
           if (filterText) {
             return ContentGroups.find({ $and: [ { name: { $regex: new RegExp(filterText), $options: 'i' } },
-                                { author: userId } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage});
+                                { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage});
           } else {
-            return ContentGroups.find({ author: userId });
+            return ContentGroups.find( { $and: [ { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage} );
           }
         } else {
-          return ContentGroups.find({ author: userId }, { sort: { name: 1 }});
+          return ContentGroups.find( { $and: [ { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] } );
         }
       }
     }
@@ -47,13 +47,12 @@ Meteor.publish('countContentGroups', function(currentPage, showPerPage, filterTe
     const roleId = Meteor.users.findOne({ _id: userId }).profile.role;
     const role = Roles.findOne({ _id: roleId });
     if (role) {
-      if (role.seeAllContentGroups || true) {
-        const networkId = role.networkId;
-        let roleIdsOfThisNetwork = Roles.find({ networkId: networkId }, { fields: { _id: 1 } }).fetch();
-        roleIdsOfThisNetwork = _(roleIdsOfThisNetwork).pluck("_id");
-        let userIdsOfThisNetwork = Meteor.users.find({ 'profile.role': { $in: roleIdsOfThisNetwork } }, { fields: { _id: 1 } }).fetch();
-        userIdsOfThisNetwork = _(userIdsOfThisNetwork).pluck("_id");
-
+      const networkId = role.networkId;
+      let roleIdsOfThisNetwork = Roles.find({ networkId: networkId }, { fields: { _id: 1 } }).fetch();
+      roleIdsOfThisNetwork = _(roleIdsOfThisNetwork).pluck("_id");
+      let userIdsOfThisNetwork = Meteor.users.find({ 'profile.role': { $in: roleIdsOfThisNetwork } }, { fields: { _id: 1 } }).fetch();
+      userIdsOfThisNetwork = _(userIdsOfThisNetwork).pluck("_id");
+      if (role.seeAllContentGroups) {
         if (currentPage && showPerPage) {
           const skip = (currentPage - 1) * showPerPage;
           if (filterText) {
@@ -70,12 +69,12 @@ Meteor.publish('countContentGroups', function(currentPage, showPerPage, filterTe
           const skip = (currentPage - 1) * showPerPage;
           if (filterText) {
             cursor =  ContentGroups.find({ $and: [ { name: { $regex: new RegExp(filterText), $options: 'i' } },
-                                { author: userId } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage});
+                                { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage});
           } else {
-            cursor =  ContentGroups.find({ author: userId });
+            cursor =  ContentGroups.find( { $and: [ { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] }, { sort: { name: 1 }, skip: skip, limit: showPerPage} );
           }
         } else {
-          cursor =  ContentGroups.find({ author: userId }, { sort: { name: 1 }});
+          return ContentGroups.find( { $and: [ { $or: [ { author: userId }, { allowedUsersIds: userId }, { visibleForAll: true } ] }, { author: { $in: userIdsOfThisNetwork } } ] } );
         }
       }
     }
